ffae0aba749fb8356ea4e160d3580b42a6ddb06e
Vasily Gnuchev <gnuchev@gmail.com>
2023-08-26 02:10:44 -0700

    frontend started - with bootstrap

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import { Container } from "react-bootstrap";
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Container className="my-2">
+      <Outlet />
+      </Container>
+    </div>
+  );
+}
+export default App;
